df7dfe8b25ae877d3c78c0c2adff314b51228972
pierre-lamarche <lamarche.p@gmail.com>
2017-07-04 00:16:26 +0200

    Figure 2

--- generate figures and graphs.R
+++ generate figures and graphs.R
@@ -11,6 +11,7 @@ library(eurostat)
 library(reshape2)
 
 col1 <- rgb(250, 165, 25, maxColorValue = 255)
+col2 <- rgb(40, 110, 180, maxColorValue = 255)
 
 #################################################################################################################################################
 ### FIGURE 1
@@ -40,3 +41,23 @@ grid(nx = NA, ny = NULL)
 axis(1,pos=0)
 axis(2,pos=0, tick = FALSE, las = 1)
 text(figure1$prop_diff, figure1$saving_rate, labels = figure1$geo, cex = 0.7, pos = 3)
+
+
+#################################################################################################################################################
+### FIGURE 2
+#################################################################################################################################################
+
+indicators_pov <- get_eurostat("icw_pov_01", time_format ="num")
+indicators_exp <- filter(indicators_pov,
+                         lev_expn == "LOW" & ind_type == "TOTAL" & lev_depr == "TOTAL" & workint == "TOTAL")
+indicators_inc <- filter(indicators_pov,
+                         lev_expn == "TOTAL" & ind_type == "ARP" & lev_depr == "TOTAL" & workint == "TOTAL")
+names(indicators_exp)[8] <- "prop_expn"
+names(indicators_inc)[8] <- "prop_inc"
+figure2 <- merge(indicators_exp[,c(6,8)], indicators_inc[,c(6,8)])
+figure2 <- figure2[order(figure2$prop_inc),]
+
+barplot(t(figure2[,2:3]), beside = TRUE, col = c(col1, col2), main = NA,
+        border = NA, legend.text = c("Low levels of expenditure", "Monetary poverty"),
+        names.arg = figure2$geo, cex.names = 0.5,
+        args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
