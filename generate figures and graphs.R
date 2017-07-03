668df98061ef34e7b5c36f066c02507370831da7
pierre-lamarche <lamarche.p@gmail.com>
2017-07-04 00:56:21 +0200

    Fgures 5 and 6

--- generate figures and graphs.R
+++ generate figures and graphs.R
@@ -96,3 +96,41 @@ barplot(t(figure4[,3:6]), beside = TRUE, col = c(col1, col2, col3, col4), main =
         border = NA, legend.text = c("Income", "Expenditures", "Savings", "Wealth"),
         names.arg = figure4$geo, cex.names = 0.5,
         args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
+
+
+#################################################################################################################################################
+### FIGURE 5
+#################################################################################################################################################
+
+indicators_dissav <- get_eurostat("icw_pov_03", time_format = "num")
+figure5 <- filter(indicators_dissav,
+                  hhtype == "NSAV")
+figure5 <- figure5[order(figure5$values),]
+
+barplot(figure5$values, col = col1, main = NA,
+        border = NA, space = 1.5,
+        names.arg = figure5$geo, cex.names = 0.5)
+
+
+#################################################################################################################################################
+### FIGURE 6
+#################################################################################################################################################
+
+indicators_sr <- get_eurostat("icw_sr_01", time_format = "num")
+indicators_agg <- get_eurostat("icw_sr_08", time_format = "num")
+
+indicators_agg <- filter(indicators_agg,
+                         age == "TOTAL")
+indicators_sr <- filter(indicators_sr,
+                        age == "TOTAL")
+indicators_agg <- mutate(indicators_agg,
+                         sr_agg = 100 - values)
+indicators_sr <- mutate(indicators_sr,
+                        sr_med = values)
+figure6 <- merge(indicators_sr[,c("geo","sr_med")], indicators_agg[,c("geo","sr_agg")])
+figure6 <- figure6[order(figure6$sr_med),]
+
+barplot(t(figure6[,2:3]), beside = TRUE, col = c(col1, col2), main = NA,
+        border = NA, legend.text = c("Median saving rate", "Aggregate saving rate"),
+        names.arg = figure6$geo, cex.names = 0.5, ylim = c(-20,50), cex.axis = 0.7,
+        args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
